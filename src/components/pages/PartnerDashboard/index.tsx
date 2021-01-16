feae38d65baf98ceea57cc4bce45fa2d99e90d6d
Tales Garcia <garcia.tales@gmail.com>
2021-01-16 19:05:33 -0300

    feat(pages): create partner dashboard

---
+++ src/components/pages/PartnerDashboard/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../shared/Header';
+
+const PartnerDashboard = React.memo(() => {
+
+    return (
+        <>
+            <Header />
+            Hello world
+        </>
+    );
+});
+
+export default PartnerDashboard;
