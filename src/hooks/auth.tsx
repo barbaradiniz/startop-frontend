aff525d4aaec3888db0d451b815f112709bdb9f8
Tales Garcia <garcia.tales@gmail.com>
2021-01-16 17:51:06 -0300

    feat(hooks): create auth hook
    Auth hook contains the user and sign up, in and out methods

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,73 @@
+import React, { useCallback, useContext, useState } from 'react';
+import { createContext } from "react";
+import api from '../services/api';
+import { uuid } from 'uuidv4';
+
+interface SignInCredentials {
+    email: string;
+    password: string;
+}
+
+interface SignUpCredentials {
+    username: string;
+    name: string;
+    type: 1 | 2;
+    email: string;
+    password: string;
+}
+
+interface AuthState {
+    user: object;
+}
+
+interface AuthContextData {
+    user: object;
+    signIn(credentials: SignInCredentials): Promise<void>;
+    signUp(credentials: SignUpCredentials): Promise<void>;
+    signOut(): void;
+}
+
+export const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+export function useAuth() {
+    const authContext = useContext(AuthContext);
+
+    if(!authContext) throw new Error('useAuth must be used within an AuthProvider');
+
+    return authContext;
+}
+
+export const AuthProvider : React.FC = ({ children }) => {
+    const [data, setData] = useState<AuthState>({} as AuthState);
+
+    const signIn = useCallback(async ({ email, password }: SignInCredentials) => {
+        const res = await api.get('users');
+
+        const user = res.data.find((user: any) => user.email === email);
+
+        if(!password === user.password) {
+            return;
+        }
+
+        setData({ user });
+    }, []);
+
+    const signOut = useCallback(() => {
+        setData({} as AuthState);
+    }, []);
+
+    const signUp = useCallback(async (data: SignUpCredentials) => {
+        Object.assign(data, { id: uuid() });
+        const res = await api.post('users', data);
+
+        const { user } = res.data;
+
+        setData({ user });
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ user: data.user, signIn, signOut, signUp }}>
+            { children}
+        </AuthContext.Provider>
+    );
+}
