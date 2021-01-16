c36e7c9d8b0264b8b0683e754cb49452f2220ad5
Tales Garcia <garcia.tales@gmail.com>
2021-01-16 17:53:35 -0300

    feat(routes): register app routes on main routes

--- src/routes/index.routes.tsx
+++ src/routes/index.routes.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import { useAuth } from '../hooks/auth';
+import AppRoutes from './app.routes';
 import AuthRoutes from './auth.routes';
 
 const Routes = React.memo(() => {
-    return (
-        <AuthRoutes />
-    );
+    return useAuth().user ? <AppRoutes /> : <AuthRoutes />;
 });
 
 export default Routes;
