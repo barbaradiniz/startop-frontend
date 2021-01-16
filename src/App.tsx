e1776349e60e70ccb8604d4756d9ac5feecc5197
Tales Garcia <garcia.tales@gmail.com>
2021-01-15 21:35:08 -0300

    feat(routes): apply routes to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import Routes from './routes/index.routes';
 
 const App: React.FC = React.memo(() => {
   return (
-    <div>
-      Hello world!
-    </div>
+    <Routes />
   );
 });
 
