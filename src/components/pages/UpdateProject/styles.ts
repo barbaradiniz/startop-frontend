f734d5eb685baed2d907c8a62f03147f5302729c
Barbara Garcia <bdsp.pe@gmail.com>
2021-01-17 16:44:25 -0300

    fix(new and create project pages): make form smaller

--- src/components/pages/UpdateProject/styles.ts
+++ src/components/pages/UpdateProject/styles.ts
@@ -18,7 +18,7 @@ export const Container = styled.div`
 
     > form {
         background-color: #3D3D3D;
-        padding: 25px 30px;
+        padding: 15px 20px;
         width: 45%;
         display: flex;
         justify-content: center;
@@ -27,7 +27,7 @@ export const Container = styled.div`
         border-radius: 10px 10px 0 0;
 
         button {
-            margin-top: 32px;
+            margin-top: 12px;
         }
     }
 `;
