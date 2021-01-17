1b1d00ad10a04a02f6df9a2779319550d562b0bc
Tales Garcia <garcia.tales@gmail.com>
2021-01-16 21:21:13 -0300

    feat(partner dashboars): create dashboard

--- src/components/pages/PartnerDashboard/index.tsx
+++ src/components/pages/PartnerDashboard/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import Header from '../../shared/Header';
+import newProject from '../../../assets/images/new-project.png';
+import { Container } from './styles';
+import ProjectCard from '../../shared/ProjectCard';
 
 const PartnerDashboard = React.memo(() => {
 
     return (
         <>
             <Header />
-            Hello world
+            <Container>
+                <h1>Seus projetos</h1>
+                <section>
+                    <ProjectCard />
+                    <ProjectCard />
+                    <ProjectCard />
+                    <img src={newProject} alt="new project"/>
+                </section>
+            </Container>
         </>
     );
 });
