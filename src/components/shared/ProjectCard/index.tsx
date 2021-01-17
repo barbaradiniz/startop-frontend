6caa19da914528daa3f0bfe4590b9585bc7cd4c3
Barbara Garcia <bdsp.pe@gmail.com>
2021-01-17 16:14:08 -0300

    feat(project card): receive props

--- src/components/shared/ProjectCard/index.tsx
+++ src/components/shared/ProjectCard/index.tsx
@@ -2,14 +2,27 @@ import React from 'react';
 import { Container, Image } from './styles';
 import image from '../../../assets/images/login-page-background.png';
 
-const ProjectCard: React.FC = React.memo(() => {
+interface IProject {
+    name: string;
+    description: string,
+    segment: string,
+    videoPitch: string,
+    businessPlan: string,
+    presentation: string,
+    investment: number
+}
+
+interface IProjectCardProps {
+    project: IProject;
+}
+
+const ProjectCard: React.FC<IProjectCardProps> = React.memo(({ project }) => {
     return (
         <Container>
             <Image src={image} />
             <div>
-                <h4><span>R$</span> 15.000</h4>
-                <h3>Projeto</h3>
-                <p>Um projeto movide de fj</p>
+                <h4><span>R$</span> {project.investment}</h4>
+                <h3>{project.name}</h3>
             </div>
         </Container>
     );
