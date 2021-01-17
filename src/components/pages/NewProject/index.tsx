c6ea2b624282049923e68165f161bb5d4c33df56
Barbara Garcia <bdsp.pe@gmail.com>
2021-01-17 17:45:09 -0300

    feat(new project): add a switch for segments

--- src/components/pages/NewProject/index.tsx
+++ src/components/pages/NewProject/index.tsx
@@ -1,11 +1,13 @@
 import { Form, Formik } from 'formik';
 import React from 'react';
 import { FiVideo, FiGrid, FiServer, FiUpload, FiAlignLeft, FiDollarSign } from 'react-icons/fi';
+import segments from '../../../config/segments';
 import { useProject } from '../../../hooks/project';
 import Button from '../../shared/Button';
 import FileInput from '../../shared/FileInput';
 import Header from '../../shared/Header';
 import InputBlock from '../../shared/InputBlock';
+import Switch from '../../shared/Switch';
 import { Container } from './styles';
 
 const NewProject = React.memo(() => {
@@ -24,7 +26,7 @@ const NewProject = React.memo(() => {
                     initialValues={{
                         name: '',
                         description: '',
-                        segment: '',
+                        segment: 3,
                         videoPitch: '',
                         businessPlan: '',
                         presentation: '',
@@ -33,9 +35,9 @@ const NewProject = React.memo(() => {
                     onSubmit={handleSubmit}
                 >
                     <Form>
+                        <Switch name="segment" data={Object.values(segments)} />
                         <InputBlock Icon={FiGrid} name="name" placeholder="Título" />
                         <InputBlock Icon={FiAlignLeft} name="description" placeholder="Descrição" textarea />
-                        <InputBlock Icon={FiServer} name="segment" placeholder="Segmento" />
                         <InputBlock Icon={FiDollarSign} name="investment" placeholder="Investimento" />
                         <FileInput Icon={FiVideo} accept="video/*" name="videoPitch" placeholder="Vídeo Pitch" />
                         <FileInput Icon={FiUpload} accept="application/pdf" name="businessPlan" placeholder="Plano de negócios (PDF)" />
