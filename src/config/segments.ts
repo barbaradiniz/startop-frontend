bc3bbe914843bf6eb661abeb238d736939f954aa
Barbara Garcia <bdsp.pe@gmail.com>
2021-01-17 17:51:13 -0300

    fix(segments config): make segments iterable

--- src/config/segments.ts
+++ src/config/segments.ts
@@ -1,7 +1,11 @@
+type IIterable = {
+    [key: string]: string;
+}
+
 export default {
     industry: 'Indústria',
     commerce: 'Comércio',
     service: 'Serviço',
     health: 'Saúde',
     others: 'Outros'
-};
+} as IIterable;
