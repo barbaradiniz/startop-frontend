2a64789c280dbe28d4432b19b733f0cc7fef8626
Tales Garcia <garcia.tales@gmail.com>
2021-01-17 11:30:35 -0300

    feat(routes): register update project route

--- src/routes/partner.routes.tsx
+++ src/routes/partner.routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import NewProject from '../components/pages/NewProject';
 import PartnerDashboard from '../components/pages/PartnerDashboard';
+import UpdateProject from '../components/pages/UpdateProject';
 
 const PartnerRoutes = React.memo(() => {
     return (
@@ -9,6 +10,7 @@ const PartnerRoutes = React.memo(() => {
             <Switch>
                 <Route exact path="/" component={PartnerDashboard} />
                 <Route path="/project/new" component={NewProject} />
+                <Route path="/project/update/:id" component={UpdateProject} />
             </Switch>
         </BrowserRouter>
     );
