74e8e4be4abb7fe8653ec61da95f18f88b11f979
Tales Garcia <garcia.tales@gmail.com>
2021-01-16 21:17:12 -0300

    feat(partner dashboard): create page style

---
+++ src/components/pages/PartnerDashboard/styles.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+    height: calc(100vh - 60px);
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+    padding-top: 60px;
+
+    > section {
+        width: 95%;
+        padding: 50px 30px;
+        display: flex;
+
+        ::-webkit-scrollbar {
+            display: none;
+        }
+
+        align-items: center;
+        overflow-x: auto;
+        justify-content: flex-start;
+        background-color: #3D3D3D;
+        border-top-right-radius: 30px;
+
+        > img {
+            width: 320px;
+            height: 320px;
+            cursor: pointer;
+        }
+
+        > *:last-child {
+            width: 352px;
+            padding-right: 32px;
+        }
+    }
+
+    > h1 {
+        margin-left: 40px;
+    }
+`;
