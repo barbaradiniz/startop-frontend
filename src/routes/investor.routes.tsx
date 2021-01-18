cc51acc7a11f06a198948c5f1cc197773207435d
Barbara Garcia <bdsp.pe@gmail.com>
2021-01-17 21:37:09 -0300

    feat(routes): surround with project provider

--- src/routes/investor.routes.tsx
+++ src/routes/investor.routes.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import InvestorDashboard from '../components/pages/InvestorDashboard';
+import { ProjectProvider } from '../hooks/project';
 
 const InvestorRoutes = React.memo(() => {
     return (
+        <ProjectProvider>
             <BrowserRouter>
                 <Switch>
-                    <Route exact path="/" component={InvestorDashboard} />
+                    <Route path="/" component={InvestorDashboard} />
                 </Switch>
             </BrowserRouter>
+        </ProjectProvider>
     );
 });
 
